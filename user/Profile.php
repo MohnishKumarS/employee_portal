e0fc75050f66ca6c204f72f35dc81964d95e8273
thineshdd <dineshddewall@gmail.com>
2024-08-17 18:48:41 +0530

    user panel

--- user/Profile.php
+++ user/Profile.php
@@ -30,7 +30,7 @@
         </button>
         <div class="collapse" id="sale-collapse" data-parent="#sidebar">
           <div class="list-group">
-            <a href="./profile.php" class="list-group-item list-group-item-action border-0 pl-5"> Profile </a>
+            <a href="#" class="list-group-item list-group-item-action border-0 pl-5">Profile</a>
             <a href="#" class="list-group-item list-group-item-action border-0 pl-5">Leave</a>
           </div>
         </div>
@@ -73,37 +73,11 @@
       </nav>
       <!-- main content -->
       <main class="container-fluid">
-        <section class="row">
-          <div class="col-md-6 col-lg-4">
-            <!-- card -->
-            <article class="p-4 rounded shadow-sm border-left mb-4">
-              <a href="#" class="d-flex align-items-center">
-                <span class="bi bi-box h5"></span>
-                <h5 class="ml-2">Ticket</h5>
-              </a>
-            </article>
-          </div>
-          <div class="col-md-6 col-lg-4">
-            <article class="p-4 rounded shadow-sm border-left mb-4">
-              <a href="#" class="d-flex align-items-center">
-                <span class="bi bi-person h5"></span>
-                <h5 class="ml-2">Leave</h5>
-              </a>
-            </article>
-          </div>
-          <div class="col-md-6 col-lg-4">
-            <article class="p-4 rounded shadow-sm border-left mb-4">
-              <a href="#" class="d-flex align-items-center">
-                <span class="bi bi-person-check h5"></span>
-                <h5 class="ml-2">Sellers</h5>
-              </a>
-            </article>
-          </div>
-        </section>
+       
         
         <div class="jumbotron jumbotron-fluid rounded bg-white border-0 shadow-sm border-left px-4">
           <div class="container">
-            <h1 class="display-4 mb-2 text-primary">Chart</h1>
+            <h1 class="display-4 mb-2 text-primary">CARD</h1>
             <p class="lead text-muted">Simple Admin Dashboard with Bootstrap.</p>
           </div>
         </div>
